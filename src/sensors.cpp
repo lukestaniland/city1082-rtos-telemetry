542c938d915a049fefe10f122a81f3d837043b4d
lukestaniland <10793597@cityplym.ac.uk>
2022-01-05 10:59:26 +0000

    steinhart equation and other changes

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -2,19 +2,20 @@
 #include "display.h"
 #include <cmath>
 
-#define THERM_GND P10_0
-#define THERM_VCC P10_3
-#define THERM_OUT P10_1
+#define THERM_GND P10_0 //Defines P10_0 as THERM_GND in code, to make code more readable
+#define THERM_VCC P10_3 //Defines P10_3 as THERM_VCC in code, to make code more readable
+#define THERM_OUT P10_1 //Defines P10_1 as THERM_OUT in code, to make code more readable
 
+ AnalogIn tempVoltage(THERM_OUT);
+ float temperatureC;
 /* Send Thread */
 
 void sendThread(void)
 {
-    // this is for reading the thermistor
-    DigitalOut ground(THERM_GND);
+    // this is for reading the thermistor. Setting ground and vcc as outputs, and tempVoltage as an input
+    DigitalOut ground(THERM_GND); 
     DigitalOut vcc(THERM_VCC);
-    AnalogIn tempVoltage(THERM_OUT);
-    // initialise the thermistor power
+    // initialise the thermistor power. Setting vcc as 1 and ground as 0, to give 3.3V across the thermistor
     vcc= true;
     ground = false;
 
@@ -24,11 +25,26 @@ void sendThread(void)
     uint32_t i = 0;
     while (true) {
         i++; // fake data update
-        temp = tempVoltage.read() * 3.3f;
-        lightLev = fmod((i * 0.1f) * 5.5f, 100);
+        temp = tempVoltage.read() * 3.3f; //f is added to floating point numbers
+        lightLev = fmod((i * 0.1f) * 5.5f, 100); //dummy value
         cycles = i;
-        displaySendUpdateSensor(temp, lightLev, cycles);
+        displaySendUpdateSensor(temp, lightLev, cycles); //sends temp, lightLev and cycles to a queue for displaying
         ThisThread::sleep_for(1s);
         
     }
+}
+
+static void readTemp()
+{
+    float temperatureF;
+    float refVoltage = tempVoltage.read() * 2.4; // Range of ADC 0->2*Vref
+    float refCurrent = refVoltage  / 10000.0; // 10k Reference Resistor
+    float thermVoltage = 3.3 - refVoltage;    // Assume supply voltage is 3.3v
+    float thermResistance = thermVoltage / refCurrent; 
+    float logrT = (float32_t)log((float64_t)thermResistance);
+    /* Calculate temperature from the resistance of thermistor using Steinhart-Hart Equation */
+    float stEqn = (float32_t)((0.0009032679) + ((0.000248772) * logrT) + 
+                             ((2.041094E-07) * pow((float64)logrT, (float32)3)));
+    temperatureC = (float32_t)(((1.0 / stEqn) - 273.15)  + 0.5);
+    temperatureF = (temperatureC * 9.0/5.0) + 32;
 }
